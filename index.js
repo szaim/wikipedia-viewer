42f677bd4a4a4a523a22cdd1c04be4cb7fcf4074
szaim <zaimsofia81@gmail.com>
2017-01-13 14:49:49 -0500

    finished project

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 $(document).ready(function(){
-    $('.fa-search').click(function() {
+    $('.fa-search').click(function(e) {
+    	e.preventDefault();
+    	$(".list-group").empty();
         $.ajax({
             url: 'http://en.wikipedia.org/w/api.php',
             data: { action: 'query', list: 'search', srsearch: $("input[name=search]").val(), format: 'json' },
@@ -11,6 +13,7 @@ $(document).ready(function(){
 });
 
   function processResult(apiResult){
+
   	for(var i = 0; i < apiResult.query.search.length; i++) {
   		$(".list-group").append("<div class='col-md-12 list'><h3>" + apiResult.query.search[i].title + "</h3>"
   								+ "<p>" + apiResult.query.search[i].snippet + "</p></div>");
